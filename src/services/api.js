08feaac5c46855ca721eceb399a121a906999fe9
Mayank2617 <mayankthesahu@gmail.com>
2025-12-06 21:04:25 +0530

    API updated final

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,23 @@
 import axios from 'axios';
 
 // Point to your running Backend Server
-const API_URL = "https://gensquadbackend.onrender.com/api/talent";
+const API_URL = 'https://gensquadbackend.onrender.com/api/talent';
 
 export const createTalent = async (formData) => {
   const response = await axios.post(API_URL, formData, {
-    headers: { 'Content-Type': 'multipart/form-data' },
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
   });
   return response.data;
 };
 
 export const updateTalent = async (id, data) => {
-  const response = await axios.put(`${API_URL}/${id}`, data);
+  const response = await axios.put(`${API_URL}/${id}`, data, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
   return response.data;
 };
 
